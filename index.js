73d1903fc11d14b0745eb9ed3ae894140e10ab97
Edward Smith <edward.m.smith80@gmail.com>
2020-08-30 05:12:12 +0000

    finished all features

--- index.js
+++ index.js
@@ -1,29 +1,90 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let total = 0;
+    for (let transaction of this.transactions) {
+      total += transaction.value;
+    }
+    return total;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
 
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    } else {
+      return;
+    }
+  }
+}
+
+class Deposit extends Transaction {
+  isAllowed() {
+    return true;
+  }
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+  isAllowed() {
+    return (this.account.balance - this.amount > 0) ? true : false;
+  }
+
+  get value() {
+    return -this.amount;
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
+console.log(`Account Balance (${myAccount.username}):`, myAccount.balance);
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log(`Account Balance (${myAccount.username}):`, myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Deposit(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log(`Account Balance (${myAccount.username}):`, myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3', t3);
+console.log(`Account Balance (${myAccount.username}):`, myAccount.balance);
+
+t4 = new Withdrawal(500.00, myAccount);
+t4.commit();
+console.log('Transaction 4:', t4);
+console.log(`Account Balance (${myAccount.username}):`, myAccount.balance);
+
+t5 = new Withdrawal(129.99, myAccount);
+t5.commit();
+console.log('Transaction 5:', t5);
+console.log(`Account Balance (${myAccount.username}):`, myAccount.balance);
+
